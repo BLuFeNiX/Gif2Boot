25515c146abaf19c7f1618700f1c4f01beb90f39
blufenix <>
2012-06-12 06:19:41 +0000

    removed unused import

--- src/backend.java
+++ src/backend.java
@@ -10,7 +10,6 @@ import java.io.FileInputStream;
 import java.io.FileOutputStream;
 import java.io.FileWriter;
 import java.io.IOException;
-import java.io.InputStream;
 import java.io.PrintWriter;
 import java.util.Iterator;
 import java.util.zip.CRC32;
